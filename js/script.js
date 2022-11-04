eb96930b88c69d2d0bc49d01ca540a44211f3fef
Adrian Płotka <adrianplotkaa@gmail.com>
2022-11-04 20:48:58 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,84 @@
+{
+  const tasks = []
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent
+    });
+
+    render();
+  };
+
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  }
+
+  const toggleTaskDone = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
+  }
+
+  const bindEvents = () => {
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index);
+      });
+    });
+
+
+    const toggleDoneButton = document.querySelectorAll(".js-done");
+
+    toggleDoneButton.forEach((toggleDoneButton, index) => {
+      toggleDoneButton.addEventListener("click", () => {
+        toggleTaskDone(index);
+      });
+    });
+  }
+
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+      <li 
+      class="list__item" >
+          <button class="list__button list__button--green js-done">${task.done ? " ✔ " : ""}</button>
+          <p class=" ${task.done ? " list__item--done " : ""}">${task.content} </p>
+          <button class="list__button list__button--red js-remove">🗑️</button>
+      </li>
+      `
+    }
+
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    bindEvents();
+  }
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskElement = document.querySelector(".js-newTask");
+    const newTaskContent = newTaskElement.value.trim();
+
+    if (newTaskContent !== "") {
+      addNewTask(newTaskContent);
+      newTaskElement.value = "";
+    }
+
+    newTaskElement.focus();
+  };
+
+  const init = () => {
+    render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
+  };
+
+  init();
+}
